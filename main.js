d23c08421f8c6540faeba940264dfbb0b6171714
simonezanfagna <zanfagnasimone@live.it>
2019-11-14 10:24:24 +0100

    modifica

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var nome = prompt("Inserisci il tuo nome");
 var cognome = prompt("Inserisci il tuo cognome");
 var colore = prompt("Inserisci il tuo colore preferito");
-document.getElementById('pw-nome').innerHTML = nome;
-document.getElementById('pw-cognome').innerHTML = cognome;
-document.getElementById('pw-colore').innerHTML = colore;
+//document.getElementById('pw-nome').innerHTML = nome;
+//document.getElementById('pw-cognome').innerHTML = cognome;
+//document.getElementById('pw-colore').innerHTML = colore;
 document.getElementById('password').innerHTML = nome + cognome + colore + 19;
